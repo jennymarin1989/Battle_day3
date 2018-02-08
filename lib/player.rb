d7f9688a9a7e0a46c611f6662780409b11370dec
jose <jennymarin1989@hotmail.com>
2018-02-08 14:56:43 +0000

    reduced points method was added and hit points instance variable

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,15 @@
 
 class Player
 
-attr_reader :name
+attr_reader :name, :hit_points
 
   def initialize(name)
     @name=name
+    @hit_points=30
   end
 
+  def reduce_hit_points
+    @hit_points -= 10
+  end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,5 +7,14 @@ describe Player do
     it "initializes with player names" do
       expect(player_1).to respond_to(:name)
     end
+    it "initializes with player hit points" do
+      expect(player_1).to respond_to(:hit_points)
+    end
+  end
+
+  context "#reduce_hit_points" do
+    it "reduces player 2 points" do
+    expect{player_1.reduce_hit_points}.to change{player_1.hit_points}.by(-10)
+    end
   end
 end
