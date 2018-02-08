4783667b9887916ea7376db21b880063013f3025
carlostrapet <trapet_llamas@hotmail.com>
2018-02-08 12:54:07 +0000

    refactored to use global variables and instances of Player class. all tests passing

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/player'
 
 
 class Battle < Sinatra::Base
@@ -11,22 +12,27 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @player_1 = params[:name_1]
-    @player_2 = params[:name_2]
-    session[:session_1]= @player_1
-    session[:session_2]= @player_2
+    $player_1 = Player.new(params[:name_1])
+    $player_2 = Player.new(params[:name_2])
+    # la global variable $ se puede asignar directamente sin
+    #pasar por instance variable @
+    # $player_1 = @player_1
+    # $player_2 = @player_2
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = session[:session_1]
-    @player_2 = session[:session_2]
+    # @player_1 = session[:session_1]
+    # @player_2 = session[:session_2]
+    # como ya tenemos la globar variable $, podemos hacer 
+    # referencia a ella directamente en los archivos html (erb)
+    # sin tener que pasar por @.... = $....
     erb :play
   end
 
   post '/attack-conf' do 
-    @player_1 = session[:session_1]
-    @player_2 = session[:session_2]
+    # @player_1 = session[:session_1]
+    # @player_2 = session[:session_2]
     erb :attack_confirm
   end
 
