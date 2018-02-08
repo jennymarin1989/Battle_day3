3f9e2e0029d91fc00d73506e0dac79099ef3c014
jose <jennymarin1989@hotmail.com>
2018-02-08 11:16:49 +0000

    Hit points display and test

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require 'sinatra/base'
-# preventing issues with sending secure session information
-#set :session_secret, 'super secret'
+
 
 class Battle < Sinatra::Base
 
  enable :sessions
+ set :public_folder, File.dirname(__FILE__) + '/static'
 
   get '/' do
     erb :index
